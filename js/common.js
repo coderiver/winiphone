255a38375924c3f562b569c24709a6aa54f681bf
dpismenny <denis.pismenny@gmail.com>
2013-06-27 16:20:16 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
 
+	var clock = new FlipClock($('.your-clock'), {
+
+	});
+	clock.setTime(235);
+	clock.setCountdown(true);
+
+	var clock = new FlipClock($('.your-clock-second'), {
+
+	});
+	clock.setTime(235);
+	clock.setCountdown(true);
+
+
+	$(".time").countdown({
+		date: "june 29, 2013 18:35",
+		onComplete: function( event ) {
+		},
+		minsOnly: true,
+		leadingZero: true
+	});
+
+	$(".time").countdown({
+		date: "june 29, 2013 18:35",
+		onComplete: function( event ) {
+		},
+		minsOnly: true,
+		leadingZero: true
+	});
+
+	//$("#time2").countdown('pause');
+	//$("#time2").countdown('resume');
 });
